1dec2940a03e64fe0154cec9d783338220886752
Adebayo <adenguk5251@gmail.com>
2024-03-18 17:03:59 +0000

    set one hour plan for each day from 9am till 6pm using js

--- script.js
+++ script.js
@@ -2,3 +2,22 @@
 var today = moment().format('dddd, MMMM D, YYYY ');
 
 var now = moment().format('H A');
+
+// current day
+
+$("#currentDay").text(today);
+
+
+// /* planforWorkday entries for one hour of the workday */
+ var planforWorkday = [
+    { time: "9 AM", event: "" },
+  { time: "10 AM", event: "" },
+       { time: "11 AM", event: "" },
+   { time: "12 PM", event: "" },
+    { time: "1 PM", event: "" },
+   { time: "2 PM", event: "" },
+     { time: "3 PM", event: "" },
+    { time: "4 PM", event: "" },
+    { time: "5 PM", event: "" },
+	   { time: "6 PM", event: "" },
+      ];
